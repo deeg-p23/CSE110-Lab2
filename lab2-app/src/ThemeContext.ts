c1eb4a0e8ca05c8a41df59509fef7538eda8ce04
Diego Pereyra <23diegopereyra@gmail.com>
2024-10-09 14:29:52 -0700

    progress

---
+++ lab2-app/src/ThemeContext.ts
@@ -0,0 +1,15 @@
+// ThemeContext.ts
+import React, { createContext } from 'react';
+
+export const themes = {
+ light: {
+   foreground: '#000000',
+   background: '#eeeeee',
+ },
+ dark: {
+   foreground: '#ffffff',
+   background: '#222222',
+ },
+};
+
+export const ThemeContext = createContext(themes.light);
